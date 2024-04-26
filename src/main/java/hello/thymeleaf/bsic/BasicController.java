26e292cfcd607024f10101a854a5ccc38ef87881
Sully <2dcoder@naver.com>
2024-04-26 21:12:42 +0900

    섹션 1: 타임리프 - 기본 기능
    리터럴

--- src/main/java/hello/thymeleaf/bsic/BasicController.java
+++ src/main/java/hello/thymeleaf/bsic/BasicController.java
@@ -80,6 +80,19 @@ public class BasicController {
         return "basic/data";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @GetMapping("literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
     @Data
     @AllArgsConstructor
     static class User {
