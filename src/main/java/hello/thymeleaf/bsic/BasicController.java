f369953305a64d8718439eefdc79ff0ccb3c9a35
Sully <2dcoder@naver.com>
2024-04-26 20:51:54 +0900

    섹션 1: 타임리프 - 기본 기능
    기본 객체들

--- src/main/java/hello/thymeleaf/bsic/BasicController.java
+++ src/main/java/hello/thymeleaf/bsic/BasicController.java
@@ -1,11 +1,15 @@
 package hello.thymeleaf.bsic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,9 +51,32 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model,
+                               HttpServletRequest request,
+                               HttpServletResponse response,
+                               HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+
+    }
+
     @Data
     @AllArgsConstructor
     static class User {
+
 
         private String username;
         private int age;
